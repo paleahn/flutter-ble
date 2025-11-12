1c5eaf35c49d992efbd6c4077d63723dd3bb57d3
Gunwoo Ahn <paleahn@arsvivendi.io>
2025-11-12 16:00:57 +0900

    T

--- android/src/main/kotlin/com/viam/ble/CentralManager.kt
+++ android/src/main/kotlin/com/viam/ble/CentralManager.kt
@@ -77,7 +77,13 @@ class CentralManager(
                             .setServiceUuid(ParcelUuid.fromString(it))
                             .build()
                     },
-                    ScanSettings.Builder().build(),
+                    ScanSettings.Builder()
+                        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                        .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
+                        .setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
+                        .setNumOfMatches(ScanSettings.MATCH_NUM_MAX_ADVERTISEMENT)
+                        .setReportDelay(0)
+                        .build(),
                     leScanCallback,
                 )
             }
